16806e46a2cbacfcc4fa2a3bdf1f6f2247df47c0
issac <issac.rajan@gmail.com>
2023-01-26 08:45:42 +0530

    init

---
+++ src/main/java/com/issac/react/TestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.issac.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBackendApplication.class, args);
+	}
+
+}
